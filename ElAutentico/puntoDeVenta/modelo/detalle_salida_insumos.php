3894536795d5833e9d36c270d2262740f2b311d4
Saliage <106983636+Saliage@users.noreply.github.com>
2023-10-13 19:49:11 +0000

    mas cruds

---
+++ ElAutentico/puntoDeVenta/modelo/detalle_salida_insumos.php
@@ -0,0 +1,71 @@
+<?php
+
+require('conexion.php');
+
+class DetalleSalidaInsumos {
+
+    // Agregar detalle de salida de insumos
+    public function agregarDetalleSalidaInsumos($salida_id, $insumos_id) {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "INSERT INTO detalle_salida_insumos (salida_id_salida, insumos_id_insumo)
+                     VALUES ('$salida_id', '$insumos_id')";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+
+    // Obtener todos los detalles de salida de insumos
+    public function listarDetallesSalidaInsumos() {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "SELECT * FROM detalle_salida_insumos";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+
+    // Buscar detalle de salida de insumos por id
+    public function buscarDetalleSalidaInsumosId($id) {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "SELECT * FROM detalle_salida_insumos WHERE id_detalle_salida = '$id'";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+
+    // Actualizar datos de detalle de salida de insumos
+    public function actualizarDetalleSalidaInsumos($id, $salida_id, $insumos_id) {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "UPDATE detalle_salida_insumos SET
+                    salida_id_salida = '$salida_id',
+                    insumos_id_insumo = '$insumos_id'
+                    WHERE id_detalle_salida = '$id'";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+
+    // Eliminar detalle de salida de insumos por id
+    public function eliminarDetalleSalidaInsumos($id) {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "DELETE FROM detalle_salida_insumos WHERE id_detalle_salida = '$id'";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+}
+?>
---
+++ ElAutentico/puntoDeVenta/modelo/detalle_venta.php
@@ -0,0 +1,72 @@
+<?php
+
+require('conexion.php');
+
+class DetalleVenta {
+
+    // Agregar detalle de venta
+    public function agregarDetalleVenta($cantidad, $id_venta, $id_producto) {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "INSERT INTO detalle_venta (cantidad, ventas_id_venta, producto_id_product)
+                     VALUES ('$cantidad','$id_venta', '$id_producto')";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+
+    // Obtener todos los detalles de venta
+    public function listarDetallesVenta() {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "SELECT * FROM detalle_venta";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+
+    // Buscar detalle de venta por id
+    public function buscarDetalleVentaId($id) {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "SELECT * FROM detalle_venta WHERE id_detalle = '$id'";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+
+    // Actualizar datos de detalle de venta
+    public function actualizarDetalleVenta($id, $id_venta, $id_producto, $cantidad) {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "UPDATE detalle_venta SET
+                    venta_id_venta = '$id_venta',
+                    producto_id_producto = '$id_producto',
+                    cantidad = '$cantidad'
+                    WHERE id_detalle = '$id'";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+
+    // Eliminar detalle de venta por id
+    public function eliminarDetalleVenta($id) {
+        $conectar = new Conexion();
+        $conn = $conectar->abrirConexion();
+
+        $consulta = "DELETE FROM detalle_venta WHERE id_detalle = '$id'";
+
+        $resultado = $conn->query($consulta);
+
+        return $resultado;
+    }
+}
+?>
