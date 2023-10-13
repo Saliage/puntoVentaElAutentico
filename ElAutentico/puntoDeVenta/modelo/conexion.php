ff5e35f2c93c8d157b46c4b37403b65e64d2c684
Saliage <106983636+Saliage@users.noreply.github.com>
2023-10-13 16:51:30 +0000

    correcciones

--- ElAutentico/puntoDeVenta/modelo/conexion.php
+++ ElAutentico/puntoDeVenta/modelo/conexion.php
@@ -7,21 +7,25 @@ class Conexion {
     private $password = "161192-4";
     private $database = "autentico";
     private $port = 3307;
+    private $conn;
 
     public function abrirConexion(){ 
         
-        $conn = mysqli_connect($this->host, $this->user, $this->password, $this->database, $this->port);
+        $this->conn = mysqli_connect($this->host, $this->user, $this->password, $this->database, $this->port);
 
         // Verificar la conexión
-        if (!$conn) {
+        if (!$this->conn) {
             die("La conexión falló: " . mysqli_connect_error());
         }
 
-        return $conn;
+        return $this->conn;
     }
 
     public function cerrarConexion() {
-        
+        // Verificar si la conexión está abierta antes de intentar cerrarla
+        if ($this->conn instanceof mysqli) {
+            mysqli_close($this->conn);
+        }
     }
 
 }
