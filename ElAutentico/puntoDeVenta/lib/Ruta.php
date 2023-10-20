c8b7c146a32987d6fff11e5de46b52052e4481d0
Saliage <saliage@hotmail.com>
2023-10-20 16:14:35 -0300

    j

--- ElAutentico/puntoDeVenta/lib/Ruta.php
+++ ElAutentico/puntoDeVenta/lib/Ruta.php
@@ -6,7 +6,7 @@ class Ruta
 {
     private static $rutas = ["/","administrador","vendedor"];
 
-    // agrega rutas de metodo GET
+/*     // agrega rutas de metodo GET
     public static function GET($uri, $callback)
     {
         $uri = substr($uri,55,null);
@@ -18,7 +18,7 @@ class Ruta
     {   
         $uri = substr($uri,55,null);
         self::$rutas['POST'][$uri] = $callback;
-    }
+    } */
         
     //recuperar URI
     public static function dispatch()
