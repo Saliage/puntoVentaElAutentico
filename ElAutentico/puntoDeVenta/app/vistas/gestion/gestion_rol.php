4b3385ef317a4e058f235a9ca10428a7fda0c701
Saliage <saliage@hotmail.com>
2023-10-31 17:47:09 -0300

    23

--- ElAutentico/puntoDeVenta/app/vistas/gestion/gestion_rol.php
+++ ElAutentico/puntoDeVenta/app/vistas/gestion/gestion_rol.php
@@ -1,52 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>CRUD de Roles</title>
-    <!-- Agrega enlaces a Bootstrap CSS y jQuery -->
-    <link rel="stylesheet" href="../../../public/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <h2>CRUD de Roles</h2>
+<?php
 
-        <!-- Formulario para agregar un nuevo rol -->
-        <form method="post" action="procesar.php">
-            <div class="form-group">
-                <label for="nombreRol">Nombre del Rol:</label>
-                <input type="text" class="form-control" id="nombreRol" name="nombreRol" required>
-            </div>
-            <button type="submit" class="btn btn-primary" name="agregarRol">Agregar Rol</button>
-        </form>
 
-        <!-- Lista de Roles -->
-        <h3>Lista de Roles</h3>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre del Rol</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                require('rol.php');
-                $rol = new Rol();
-                $result = $rol->listarRoles();
-                while ($row = $result->fetch_assoc()) {
-                    echo '<tr>';
-                    echo '<td>' . $row['id_rol'] . '</td>';
-                    echo '<td>' . $row['nombre_rol'] . '</td>';
-                    echo '<td>
-                            <a href="editar.php?id=' . $row['id_rol'] . '">Editar</a>
-                            <a href="eliminar.php?id=' . $row['id_rol'] . '">Eliminar</a>
-                          </td>';
-                    echo '</tr>';
-                }
-                ?>
-            </tbody>
-        </table>
-    </div>
-</body>
-</html>
+include("../puntoVentaElAutentico/ElAutentico/puntoDeVenta/app/modelo/rol.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verificar si el campo de texto está presente en el formulario
+    if (isset($_POST["textoInput"])) {
+        // Obtener el valor del campo de texto
+        $nombreRol = $_POST["textoInput"];
+
+        $rol = new Rol();
+        $resultado = $rol->agregarRol($nombreRol);
+
+        echo "se agregó el rol: ".$resultado;
+        
+    } else {
+        // El campo de texto no está presente en el formulario
+        echo "Error: El campo de texto no está presente en el formulario.";
+    }
+} else {
+    // Acceso incorrecto al script
+    echo "Acceso incorrecto al script.";
+}
+
+
+?>
---
+++ ElAutentico/puntoDeVenta/app/vistas/gestion/gestion_trabajador.php
@@ -0,0 +1,31 @@
+<?php
+include("../puntoVentaElAutentico/ElAutentico/puntoDeVenta/app/modelo/trabajador.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $rut = $_POST["rut"];
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $usuario = $_POST["usuario"];
+    $clave = $_POST["clave"];
+    $activo = $_POST["activo"];
+    $rol_id = $_POST["rol_id"];
+
+    // Llama al método agregarTrabajador con los datos del formulario
+    $trabajador = new Trabajador();
+    $resultado = $trabajador->agregarTrabajador($rut, $nombre, $apellido, $usuario, $clave, $activo, $rol_id);
+
+    if ($resultado) {
+        echo "Trabajador agregado exitosamente.";
+    } else {
+        echo "Error al agregar el trabajador.";
+    }
+}
+else
+{
+    header("location: ../formularios/agregar_trabajador.html"); 
+    die();
+
+}
+
+
+?>
