c662bc9eb51447074243433c81897f7438c72a94
Saliage <saliage@hotmail.com>
2023-11-02 17:11:23 -0300

    creación y config. de usuario en BD

--- ElAutentico/puntoDeVenta/app/modelo/conexion.php
+++ ElAutentico/puntoDeVenta/app/modelo/conexion.php
@@ -3,8 +3,8 @@
 class Conexion {
 
     private $host = "localhost";
-    private $user = "root";
-    private $password = "admin";
+    private $user = "autentico";
+    private $password = "autentico";
     private $database = "autentico";
     private $port = 3306;
     private $conn;
