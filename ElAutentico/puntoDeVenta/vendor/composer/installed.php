a6303a75fec721681c290950fca54738d5d75b7a
Saliage <saliage@hotmail.com>
2023-11-17 00:23:13 -0300

    +cosas

--- ElAutentico/puntoDeVenta/vendor/composer/installed.php
+++ ElAutentico/puntoDeVenta/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '31646afb142eaee40f8f7aa63c348fc7df24e385',
+        'reference' => '1c032dde31ff8c21739e4693e8e2513148dd2580',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '31646afb142eaee40f8f7aa63c348fc7df24e385',
+            'reference' => '1c032dde31ff8c21739e4693e8e2513148dd2580',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
